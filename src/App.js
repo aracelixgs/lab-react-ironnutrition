e2c34d0f9c12c7bc56572c4523afa3d9bbfa9989
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-03-01 16:50:10 +0100

    Iteración 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+//import { Card, Row, Col, Divider, Input, Button } from "antd";
+import foods from "./foods.json";
 
 function App() {
+
+const foodArr = foods
+//console.log(simpleFood)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Food List</h2>
+
+      <div>
+        {foodArr.map((eachFood) => {
+        return (
+          <tbody key={eachFood.name}>
+          <h3>{eachFood.name}</h3>
+          <img
+          src={eachFood.image}
+          alt={eachFood.name}
+          width="200px" />
+          </tbody>
+        )
+      })}
+
+      </div>
+      
     </div>
   );
 }
